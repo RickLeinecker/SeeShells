f063536060f4552ad5262da2bf2a19b5a15cffbe
Aleksandar Stoyanov <alexsd6@Knights.ucf.edu>
2020-01-10 14:32:38 -0500

    ShellBagParser skeleton

---
+++ WPF/SeeShells/SeeShells/ShellParser/ShellBagParser.cs
@@ -0,0 +1,27 @@
+﻿using SeeShells.ShellParser.ShellItems;
+using System.Collections.Generic;
+
+namespace SeeShells.ShellParser
+{
+    /// <summary>
+    /// This class is used to obtain and identify ShellBag items. 
+    /// </summary>
+    class ShellBagParser
+    {
+        IRegistryReader registryReader;
+        public ShellBagParser(IRegistryReader registryReader)
+        {
+            this.registryReader = registryReader;
+        }
+
+        /// <summary>
+        /// Identifies and gathers ShellBag items from raw binary registry data.
+        /// </summary>
+        /// <returns>a list of different variety ShellBag items</returns>
+        public List<IShellItem> GetShellItems()
+        {
+            // TODO: actual logic
+            return new List<IShellItem>();
+        }
+    }
+}
