20e8782ed738f2a4c7bd150cd1e386751e26b41e
Klayton Killough <klaki892@gmail.com>
2020-01-17 20:10:55 -0500

    Adds IEvent Interface

---
+++ WPF/SeeShells/SeeShells/UI/IEvent.cs
@@ -0,0 +1,37 @@
+﻿using SeeShells.ShellParser.ShellItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeShells.UI
+{
+    /// <summary>
+    /// A Discrete time-based event that is derived from a ShellBag.
+    /// </summary>
+    interface IEvent
+    {
+        /// <summary>
+        /// Identifier for the entity which was modified at this particular <see cref="EventTime"/>
+        /// (e.g. "C:\", "Minecraft.zip") 
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Details the time in which an event took place.
+        /// </summary>
+        DateTime EventTime { get; }
+
+        /// <summary>
+        /// The ShellItem which this event was derived from.
+        /// Provides optional extra information to the formulation of this event.
+        /// </summary>
+        IShellItem Parent { get; }
+
+        /// <summary>
+        /// Categorizes the action which was preformed.
+        /// </summary>
+        string EventType { get; }
+    }
+}
