ba8522634eebea5d1f8477e3d6a5c4462e0e2582
Aleksandar Stoyanov <alexsd6@yahoo.com>
2020-01-20 14:11:55 -0500

    A skeleton of CSV IO (HTML IO should be similar)

---
+++ WPF/SeeShells/SeeShellsTests/IO/CsvIOTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeShells.IO;
+
+namespace SeeShellsTests.IO
+{
+    class CsvIOTests
+    {
+        /// <summary>
+        /// Tests if a CSV file is otputted.
+        /// </summary>
+        [TestMethod()]
+        public void OutputCSVFileTest()
+        {
+
+        }
+
+        /// <summary>
+        /// Tests if a CSV file is imported, parsed and converted to a list of ShellItems.
+        /// </summary>
+        [TestMethod()]
+        public void ImportCSVFileTest()
+        {
+
+        }
+    }
+}
