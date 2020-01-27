f90ab09f465d375f7078e80c7c3a5e1dcfd8b7a4
yara-58 <yara-58@users.noreply.github.com>
2020-01-27 18:13:14 -0500

    Fixed Everything i think

---
+++ WPF/SeeShells/SeeShellsTests/UI/EventCollectionTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeShells;
+using SeeShells.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeShellsTests.UI
+{
+    [TestClass()]
+    class EventCollectionTest
+    {
+        [TestMethod()]
+        public void EventCollectionTests()
+        {
+            List<IEvent> eventList = new List<IEvent>();
+            Event aEvent = new Event("item1", DateTime.Now, null, "Access");
+            eventList.Add(aEvent);
+            App.eventCollection.eventList = eventList;
+            Assert.AreNotEqual(App.eventCollection.eventList.Count, 0);
+
+        }
+    }
+}
