f669cfcc92de23fe386b7ec1266b55721af62e0f
Szczura <frackie.sara@gmail.com>
2020-02-15 16:48:26 -0500

    base code

---
+++ WPF/SeeShells/SeeShells/ShellParser/Scripting/LuaShellItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeeShells.ShellParser.ShellItems;
+using NLua;
+
+namespace SeeShells.ShellParser.Scripting
+{
+    public class LuaShellItem : IShellItem
+    {
+        private readonly Lua state = new Lua();
+
+        public ushort Size => throw new NotImplementedException();
+
+        public byte Type => throw new NotImplementedException();
+
+        public string TypeName => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public DateTime ModifiedDate => throw new NotImplementedException();
+
+        public DateTime AccessedDate => throw new NotImplementedException();
+
+        public DateTime CreationDate => throw new NotImplementedException();
+
+        public IDictionary<string, string> GetAllProperties()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF/SeeShells/SeeShells/ShellParser/Scripting/ScriptHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeeShells.ShellParser.ShellItems;
+
+namespace SeeShells.ShellParser.Scripting
+{
+
+    public class ScriptHandler
+    {
+        /// <summary>
+        /// A dictionary of key value pairs to store the scripts.
+        /// The key is an int for the shell item identifier.
+        /// The value is a string for the content of the script.
+        /// </summary>
+        private readonly Dictionary<int, string> scripts = new Dictionary<int, string>();
+
+        public ScriptHandler()
+        {
+            GetScripts();
+        }
+
+        public bool HasScriptForShellItem(int identifier)
+        {
+            return scripts.ContainsKey(identifier);
+        }
+
+        public IShellItem ParseShellItem(int identifier)
+        {
+            scripts.TryGetValue(identifier, out string script);
+
+            // make lua shell item with the script
+            // return the lua shell item
+
+            throw new NotImplementedException();
+        }
+
+        private void GetScripts()
+        {
+            // get scripts from the configuration file & store them in scripts dictionary
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
+
