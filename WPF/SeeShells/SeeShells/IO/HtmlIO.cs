2d2a7222749532b5164f084af8eb2af89ee3c5bb
Bridget Woodye <BrdgtColleenW@gmail.com>
2020-02-18 12:13:49 -0500

    HTML Output
    This creates the HTML File of the filtered timeline.

---
+++ WPF/SeeShells/SeeShells/IO/HtmlIO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SeeShells.ShellParser.ShellItems;
+using SeeShells.UI;
+
+namespace SeeShells.IO
+{
+    public static class HtmlIO
+    {
+        /// <summary>
+        /// Creates a HTML file with ShellBag data in a given location.
+        /// </summary>
+        /// <param name="eventList">A list of IEvents containg the filtered list of events from the timeline</param>
+        /// <param name="filePath">The directory in which to output the CSV file including the CSV file name</param>
+        /// <returns></returns>
+        public static void OutputHtmlFile(List<IEvent> eventList, String filePath)
+        {
+            // Creates the html file
+            StreamWriter outputFile;
+            outputFile = File.CreateText(filePath);
+            outputFile.WriteLine("<html>");
+
+            // Implements the CSS Styling
+            outputFile.WriteLine("<head>");
+            outputFile.WriteLine("<style>");
+            outputFile.WriteLine(".card {box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);transition: 0.3s;width: 80 %;}");
+            outputFile.WriteLine(".card:hover {box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);}");
+            outputFile.WriteLine(".container {padding: 2px 16px;}");
+            outputFile.WriteLine(".grid-container {display: grid;grid-template-columns: 25% 75%;padding: 10px;}");
+            outputFile.WriteLine(".grid-item {padding: 10px;border-left: 1px solid rgba(0, 0, 0, 0.8);text-align: center;}");
+            outputFile.WriteLine("</style>");
+            outputFile.WriteLine("</head>");
+
+            // Adding the shellItems to the body of thr html
+            outputFile.WriteLine("<body>");
+            outputFile.WriteLine("<h2>Timeline HTML Output</h2>");
+
+            // Iterates through the list of IEvents and adds the information to the HTML file
+            foreach (IEvent aEvent in eventList)
+            {
+                outputFile.WriteLine("<div class=\"card\">");
+                outputFile.WriteLine("<div class=\"container\">");
+                outputFile.WriteLine("<div class=\"grid-container\">");
+                // This is the opening of the main shellItem
+                outputFile.WriteLine("<div class=\"grid-item\">");
+                outputFile.WriteLine("<h4><b>" + aEvent.Name + "</b></h4>");
+                outputFile.WriteLine("<p>" + aEvent.EventTime + "</p>");
+                outputFile.WriteLine("<p>" + aEvent.EventType + "</p>");
+                outputFile.WriteLine("</div>");
+                
+                // This shows the extra information about the shellItem
+                outputFile.WriteLine("<div class=\"grid-item\" style=\"column-count: 3;\" > ");
+                foreach (KeyValuePair<string, string> property in aEvent.Parent.GetAllProperties())
+                {
+                    outputFile.WriteLine("<p>" + property + "</p>");
+                }
+
+                outputFile.WriteLine("</div>");
+                outputFile.WriteLine("</div>");
+                outputFile.WriteLine("</div>");
+                outputFile.WriteLine("</div>");
+            }
+
+            outputFile.WriteLine("</body>");
+            outputFile.WriteLine("</html>");
+            outputFile.Close();
+        }
+    }
+}
---
+++ WPF/SeeShells/SeeShellsTests/IO/HtmlIOTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeShells.IO;
+using SeeShells.ShellParser;
+using SeeShells.ShellParser.ShellItems;
+using SeeShells.UI;
+using SeeShellsTests.ShellParser.ShellParserMocks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SeeShellsTests.IO
+{
+    [TestClass]
+    public class HtmlIOTests
+    {
+        /// <summary>
+        /// Tests if a CSV file is otputted.
+        /// </summary>
+        [TestMethod()]
+        public void OutputHtmlFileTest()
+        {
+            List<IEvent> eventList = new List<IEvent>();
+
+            Dictionary<string, string> shellItemProperties = new Dictionary<string, string>();
+            shellItemProperties.Add("Size", "0");
+            shellItemProperties.Add("Type", "31");
+            shellItemProperties.Add("TypeName", "Some Type Name");
+            shellItemProperties.Add("Name", "Some Name");
+            shellItemProperties.Add("ModifiedDate", "1/1/0001 12:00:00 AM");
+            shellItemProperties.Add("AccessedDate", "1/1/0001 12:00:00 AM");
+            shellItemProperties.Add("CreationDate", "1/1/0001 12:00:00 AM");
+            CsvParsedShellItem ShellItem = new CsvParsedShellItem(shellItemProperties);
+
+            Event aEvent = new Event("item1", DateTime.Now, ShellItem, "Access");
+            eventList.Add(aEvent);
+
+            if (File.Exists("timeline.html"))
+            {
+                File.Delete("timeline.html");
+            }
+            HtmlIO.OutputHtmlFile(eventList, "timeline.html");
+            Assert.IsTrue(File.Exists("timeline.html"));
+        }
+    }
+}
