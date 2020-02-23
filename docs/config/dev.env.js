dfc263278d3f5c35d428d2bda01c9ede9ef40dbf
yara-58 <yara-58@users.noreply.github.com>
2020-02-23 14:57:19 -0500

    possible fixes

--- docs/config/dev.env.js
+++ docs/config/dev.env.js
@@ -2,6 +2,11 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
+//module.exports = merge(prodEnv, {
+//  NODE_ENV: '"development"'
+//})
+module.exports = {
+    PublicPath: process.env.NODE_ENV === 'production'
+        ? '/SeeShells/'
+        : '/'
+}
