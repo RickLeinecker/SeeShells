b6c033cb04164ea3b628353b2d4bcc428b89fb50
Klayton Killough <klaki892@gmail.com>
2020-03-01 21:31:04 -0500

    Adds default configuration support to WPF
    - Adds embedded OS Configuration file
    - Adds Prompts for user confirmation on missing/invalid configs

--- WPF/SeeShells/SeeShells/ShellParser/ConfigParser.cs
+++ WPF/SeeShells/SeeShells/ShellParser/ConfigParser.cs
@@ -1,35 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Newtonsoft.Json;
 using SeeShells.IO.Networking.JSON;
 using SeeShells.UI.ViewModels;
 using Microsoft.Win32;
+using NLog;
 using SeeShells.ShellParser.Scripting;
+using SeeShells.UI.Pages;
 
 namespace SeeShells.ShellParser
 {
     class ConfigParser : IConfigParser
     {
         private readonly string OSRegistryFile;
+        private const string DefaultOsConfig = "defaultOS.json";
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
 
         public string OsVersion {private get; set;}
 
-        public ConfigParser(string guidsFile, string OSRegistryFile)
+        public ConfigParser(string guidsFile = "", string OSRegistryFile = "", string scriptsFile = "")
         {
-            UpdateKnownGUIDS(guidsFile);
-            OsVersion = getLiveOSVersion();
-            this.OSRegistryFile = OSRegistryFile;
-        }
-        /// <summary>
-        /// Scripts are optional, so use this constructor when there is a selected scripts file.
-        /// </summary>
-        /// <param name="guidsFile"></param>
-        /// <param name="OSRegistryFile"></param>
-        /// <param name="scriptsFile"></param>
-        public ConfigParser(string guidsFile, string OSRegistryFile, string scriptsFile)
-        {
+            guidsFile = IsValidGuidFile(guidsFile) ? guidsFile : string.Empty;
+            OSRegistryFile = IsValidOsFile(OSRegistryFile) ? OSRegistryFile : string.Empty;
+            scriptsFile = IsValidScriptFile(scriptsFile) ? scriptsFile : string.Empty;
+
             UpdateKnownGUIDS(guidsFile);
             UpdateScripts(scriptsFile);
             OsVersion = getLiveOSVersion();
@@ -44,6 +43,9 @@ namespace SeeShells.ShellParser
         /// <param name="guidsFile">A JSON file that contains <see cref="GUIDPair"/></param>
         private void UpdateKnownGUIDS(string guidsFile)
         {
+            if (guidsFile.Equals(string.Empty))
+                return;
+
             IList<GUIDPair> guidPairs = JsonConvert.DeserializeObject<IList<GUIDPair>>(File.ReadAllText(guidsFile));
             foreach(GUIDPair pair in guidPairs)
             {   
@@ -54,6 +56,9 @@ namespace SeeShells.ShellParser
 
         private void UpdateScripts(string file)
         {
+            if (file.Equals(string.Empty))
+                return;
+
             IList<DecodedScriptPair> scriptPairs = JsonConvert.DeserializeObject<IList<DecodedScriptPair>>(File.ReadAllText(file));
             foreach(DecodedScriptPair pair in scriptPairs)
             {
@@ -67,7 +72,10 @@ namespace SeeShells.ShellParser
         {
             List<string> locations = new List<string>();
 
-            IList<RegistryLocations> registryLocations = JsonConvert.DeserializeObject<IList<RegistryLocations>>(File.ReadAllText(OSRegistryFile));
+            IList<RegistryLocations> registryLocations = OSRegistryFile.Equals(string.Empty)
+                ? GetDefaultRegistryLocations()
+                : JsonConvert.DeserializeObject<IList<RegistryLocations>>(File.ReadAllText(OSRegistryFile));
+
             foreach (RegistryLocations regLocation in registryLocations)
             {
                 if (OsVersion.Contains(regLocation.OperatingSystem))
@@ -96,6 +104,78 @@ namespace SeeShells.ShellParser
         {
             RegistryKey registryKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion");
             return (string)registryKey.GetValue("productName");   
+        }
+
+
+        /// <summary>
+        /// Retrieves the default <see cref="RegistryLocations"/> configuration from the embedded resource.
+        /// </summary>
+        /// <returns>A list of Registry Locations.</returns>
+        public static IList<RegistryLocations> GetDefaultRegistryLocations()
+        {
+            IList<RegistryLocations> retval = new List<RegistryLocations>();
+            try
+            {
+                //internal resource retrieval, see: https://stackoverflow.com/a/3314213
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                string internalResourcePath = assembly.GetManifestResourceNames().Single(str => str.EndsWith(DefaultOsConfig));
+                using (Stream fileStream = assembly.GetManifestResourceStream(internalResourcePath))
+                {
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        retval = JsonConvert.DeserializeObject<IList<RegistryLocations>>(reader.ReadToEnd());
+                    }
+                }
+            }
+            catch (JsonSerializationException)
+            {
+                //this should never happen. if it does, we are somehow missing internal (compiled) resources or its malformed.
+                logger.Fatal("Cannot load default OS Configuration file. Internal resource is missing or broken." +
+                             " Please notify the developers with your error log and browse for a new OS Configuration file.");
+            }
+
+            return retval;
+        }
+
+        /// <summary>
+        /// Checks if a JSON file can be deserialized for a valid known Type
+        /// </summary>
+        /// <typeparam name="T">The known API Type to deserialize from. See <see cref="IO.Networking.JSON"/></typeparam>
+        /// <param name="location">a fully qualified path to a json file</param>
+        /// <returns>true, if the file could successfully be deserialized. False otherwise.</returns>
+        private static bool IsValidConfigFile<T>(string location)
+        {
+            if (File.Exists(location))
+            {
+                string json = File.ReadAllText(location);
+                try
+                {
+                    JsonConvert.DeserializeObject<T> (json);
+                    //if we can deserialize, we can use it.
+                    return true;
+                }
+                catch (JsonSerializationException ex)
+                {
+                    logger.Error(ex);
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsValidOsFile(string location)
+        {
+            return IsValidConfigFile<IList<RegistryLocations>>(location);
+        }
+        public static bool IsValidGuidFile(string location)
+        {
+            return IsValidConfigFile<IList<GUIDPair>>(location);
+        }
+
+        public static bool IsValidScriptFile(string location)
+        {
+            return IsValidConfigFile<IList<DecodedScriptPair>>(location);
         }
 
     }
