8c353e9f2cc2ec36a4c7367e11ba4d8cd2b5de04
szczura <38367751+szczura@users.noreply.github.com>
2020-03-05 17:12:05 -0500

    Website rework (#202)
    * Moved folders around to accommodate Vue and Github pages.
    
    * Updated Website Test action - This action also handles building the production files in the docs folder now for Github pages.
    
    * Base UI page for registration.
    
    * Modified files to use vue-router to allow for navigation

---
+++ website/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="home">
+        <p>home!!!</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
---
+++ website/src/components/NavigationBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="nav">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <img alt="Vue logo" src="./../assets/logo.png" width="75" height="75">
+            <router-link to='/SeeShells/'><b-navbar-brand>SeeShells</b-navbar-brand></router-link>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">About</b-nav-item>
+                    <b-nav-item href="#">Download the Program</b-nav-item>
+                    <b-nav-item href="#">Developer Team</b-nav-item>
+                    <b-nav-item href="https://github.com/RickLeinecker/SeeShells">GitHub Page</b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template v-slot:button-content>
+                            <em>Administrative</em>
+                        </template>
+                        <b-dropdown-item><router-link to='/SeeShells/register'>Register</router-link></b-dropdown-item>
+                        <b-dropdown-item href="#">Login</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavigationBar'
+    }
+</script>
+
+<style scoped>
+    #nav{
+          margin-left: 0px;
+          margin-right: 0px;
+    }
+</style>
