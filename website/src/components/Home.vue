5271f6080e0527b8f12b0af7209447788b02414d
yara-58 <yara-58@users.noreply.github.com>
2020-03-08 17:50:33 -0400

    Added content to Home Page.

--- website/src/components/Home.vue
+++ website/src/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
     <div id="home">
-        <p>home!!!</p>
+        <h1>Welcome to SeeShells</h1>
+        <h6>Extract Information - Create Timeline - Filter your information - Export Report or Raw data</h6>
+        <button type="button">Download SeeShells.exe</button>
+        <p/>
+        <p>
+            SeeShells is essentially an information extraction software.
+            The objective is to create a standalone open source executable that can run both online and offline.
+            It will extract and parse through Windows Registry information. This data will then be converted into two forms.
+            The first is a csv file that will contain all the raw data we obtain. The second is a human readable timeline.
+            The timeline will provide an interactive easier to read visualization of the data extracted from the windows registries,
+            which is otherwise difficult and time consuming to comb through and understand.
+        </p>
     </div>
 </template>
 
 <script>
-
 export default {
   name: 'Home'
 }
