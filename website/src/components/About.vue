4d0e234da60d8b63c58dd74637377aaed58ce664
yara-58 <yara-58@users.noreply.github.com>
2020-03-08 23:49:04 -0400

    fixed styling for About page and added some content

--- website/src/components/About.vue
+++ website/src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="home">
         <h1>About Our Project</h1>
-        <p>
+        <p align="left">
             SeeShells is essentially an information extract software. The objective is to
             create a standalone open source executable that can run both online and offline.
             It will extract and parse through Windows Registry information. This data will then
@@ -14,13 +14,34 @@
             Windows 7,8,8.1 and 10. In order to create a robust application we have set up a
             server to store database information on parsing different registry versions.
         </p>
-        <p>
+        <p align="left">
             In the long run our hope for this software is that it will expedite the process
             of extracting, parsing, and presenting the registry information in a way that is
             condensed and easily understandable. We hope others will benefit from our
             interactive timeline generated from the ShellBag information and we hope to make
             a great impact on the digital forensics community.
         </p>
+        <p/>
+        <h2 align="left">Objectives of SeeShells</h2>
+        <ul>
+            <li align="left">
+                Create a standalone WPF application that can extract and parse ShellBags
+                registry information.
+            </li>
+            <li align="left">
+                Support multiple Windows Operating Systems as well as reading both live
+                system registries and offline hives.
+            </li>
+            <li align="left">
+                Draw a human-readable, graphical timeline for users to look at as well as a
+                document that can be inserted into a technical report.
+            </li>
+            <li align="left">
+                Set up and utilize a server to store database information on parsing different
+                registry versions to create a robust application.
+            </li>
+            
+        </ul>
     </div>
 </template>
 
