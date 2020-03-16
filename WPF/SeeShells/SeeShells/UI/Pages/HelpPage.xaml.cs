faf7139e7a133e8163d7418d2fe98809ac778ec3
Klayton Killough <klaki892@gmail.com>
2020-03-16 03:16:05 -0400

    Adds Proof of Concept Markdown rendering for WPF Help
    - Adds Package and Window showing Markdown integration
    - Adds basic Readme file to WPF
    - Adds offline readme file skeleton

--- WPF/SeeShells/SeeShells/UI/Pages/HelpPage.xaml.cs
+++ WPF/SeeShells/SeeShells/UI/Pages/HelpPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xaml;
+using Markdig.Renderers;
+using Newtonsoft.Json;
+using SeeShells.IO.Networking.JSON;
+using XamlReader = System.Windows.Markup.XamlReader;
 
 namespace SeeShells.UI.Pages
 {
@@ -20,9 +28,40 @@ namespace SeeShells.UI.Pages
     /// </summary>
     public partial class HelpPage : Page
     {
+        private const string ReadmeFile = "README.md";
+
         public HelpPage()
         {
             InitializeComponent();
+            Loaded += OnLoad;
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+
+            string markdown;
+
+            //todo use API call for getting help, if fails use internal resource.
+
+            //internal resource retrieval, see: https://stackoverflow.com/a/3314213
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string internalResourcePath = assembly.GetManifestResourceNames().Single(str => str.EndsWith(ReadmeFile));
+            using (Stream fileStream = assembly.GetManifestResourceStream(internalResourcePath))
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    markdown = reader.ReadToEnd();
+                }
+            }
+
+
+            HelpViewer.Markdown = markdown;
+        }
+
+        private void OpenHyperlink(object sender, ExecutedRoutedEventArgs e)
+        { 
+            Process.Start(e.Parameter.ToString());
+
         }
     }
 }
