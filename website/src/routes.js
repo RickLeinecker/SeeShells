19c2d1a7939d997bf0a548c8dac65a74be8c70fc
Szczura <frackie.sara@gmail.com>
2020-03-25 21:43:52 -0400

    Add new OS page

--- website/src/routes.js
+++ website/src/routes.js
@@ -4,6 +4,7 @@ import Login from './components/LoginForm.vue';
 import Scripts from './components/ScriptPage.vue';
 import ApproveUsers from './components/ApproveUserPage.vue';
 import GUIDPage from './components/GUIDPage.vue';
+import OSPage from './components/OSPage.vue';
 
 const root = '/SeeShells/'
 
@@ -13,7 +14,8 @@ const routes = [
     { path: root + 'login', component: Login },
     { path: root + 'scripts', component: Scripts },
     { path: root + 'approveusers', component: ApproveUsers },
-    { path: root + 'guids', component: GUIDPage }
+    { path: root + 'guids', component: GUIDPage },
+    { path: root + 'os', component: OSPage }
 ];
 
 export default routes;
