03591f6c81819f5f8277e18cc3653ee26e60b29d
Bridget Woodye <BrdgtColleenW@gmail.com>
2020-03-30 22:18:07 -0400

    Label Added When there are no Nodes

--- WPF/SeeShells/SeeShells/UI/Pages/TimelinePage.xaml.cs
+++ WPF/SeeShells/SeeShells/UI/Pages/TimelinePage.xaml.cs
@@ -189,6 +189,7 @@ namespace SeeShells.UI.Pages
             {
                 if(App.nodeCollection.nodeList.Count == 0)
                 {
+                    System.Windows.MessageBox.Show("Hit");
                     logger.Info("No nodes to draw on the timeline.");
                     return;
                 }
@@ -222,8 +223,13 @@ namespace SeeShells.UI.Pages
 
                 if (nodeList.Count == 0)
                 {
+                    EmptyTimeline.Visibility = Visibility.Visible;
                     logger.Info("All nodes are filtered out, no nodes to draw on the timeline.");
                     return;
+                }
+                else
+                {
+                    EmptyTimeline.Visibility = Visibility.Collapsed;
                 }
 
                 List<Node.Node> nodesCluster = new List<Node.Node>(); // Holds events for one timeline at a time.
