e06311045e63af87933d4e7944d63e00dd321e47
szczura <38367751+szczura@users.noreply.github.com>
2020-04-06 16:01:46 -0400

    Navigation/refresh doesn't display 404 page. (#316)
    * Added 404 hack method.
    
    add doc folder
    
    Try different fix
    
    Update packages
    
    added seeshells to path
    
    * Update lock and references
    
    * Add docs folder with the build files
    
    Co-authored-by: GitHub Action <action@github.com>

--- website/src/main.js
+++ website/src/main.js
@@ -4,9 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueRouter from 'vue-router'
 import VueSession from 'vue-session'
+import Vuex from 'vuex'
+import store from 'vuex'
 import VueShowdown from 'vue-showdown'
 import App from './App.vue'
 import routes from './routes';
+
+Vue.use(Vuex) 
 
 Vue.config.productionTip = true
 Vue.prototype.$baseurl = (Vue.config.productionTip) ? 'https://seeshells.herokuapp.com/' : 'http://localhost:3000/'
@@ -21,5 +25,13 @@ const router = new VueRouter({ mode: 'history', routes });
 
 new Vue({
     router,
-    render: h => h(App)
+    store,
+    render: h => h(App),
+    created() {
+        if (sessionStorage.redirect) {
+            const redirect = sessionStorage.redirect
+            delete sessionStorage.redirect
+            this.$router.push(redirect)
+        }
+    }
 }).$mount('#app');
