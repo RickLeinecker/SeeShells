615403a62da6123f74344b0471263a3539d6c135
yara-58 <yara-58@users.noreply.github.com>
2020-04-06 13:12:16 -0400

    Trace Level log information

--- WPF/SeeShells/SeeShells/App.xaml.cs
+++ WPF/SeeShells/SeeShells/App.xaml.cs
@@ -73,8 +73,8 @@ namespace SeeShells
             var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
 
             // Rules for mapping loggers to targets            
-            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
-            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
 
             // Apply config           
             NLog.LogManager.Configuration = config;
